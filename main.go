a41193fe1a6034ee07fb5444eff493a66cbe78c3
mocha8686 <jericho.paulino@yahoo.com.ph>
2023-12-29 18:54:51 -0800

    echo server

--- main.go
+++ main.go
@@ -1,9 +1,54 @@
 package main
 
 import (
-	"fmt"
+	"io"
+	"log/slog"
+	"net"
+	"os"
 )
 
 func main() {
-	fmt.Println("Hello, world!")
+	ln, err := net.Listen("tcp", ":8000")
+	if err != nil {
+		slog.Error("Failed to create listener", "error", err)
+		os.Exit(1)
+	}
+
+	for {
+		conn, err := ln.Accept()
+		if err != nil {
+			slog.Error("Failed to accept connection", "error", err)
+			continue
+		}
+
+		slog.Info("Client connected")
+		go handleConnection(conn)
+	}
+}
+
+func handleConnection(c net.Conn) {
+	defer c.Close()
+
+	buf := make([]byte, 1024)
+	for {
+		n, err := c.Read(buf)
+		if err != nil {
+			if err == io.EOF {
+				slog.Info("Client disconnected")
+				return
+			}
+
+			slog.Warn("Failed to read client data", "error", err)
+			return
+		}
+
+		msg := buf[:n]
+		slog.Info("Message received from client", "msg", string(msg))
+
+		_, err = c.Write(msg)
+		if err != nil {
+			slog.Warn("Failed to write to client", "error", err)
+			return
+		}
+	}
 }
