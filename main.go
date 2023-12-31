9dfde31caf0f3f5db8f50e459b90cc8b1e99a788
mocha8686 <jericho.paulino@yahoo.com.ph>
2023-12-30 20:30:21 -0800

    get port from env

--- main.go
+++ main.go
@@ -2,9 +2,12 @@ package main
 
 import (
 	"log/slog"
+	"strconv"
 
 	"github.com/rs/zerolog"
 	slogzerolog "github.com/samber/slog-zerolog/v2"
+
+	"os"
 
 	"github.com/mocha8686/oc_server/internal"
 )
@@ -16,7 +19,19 @@ func main() {
 		Logger: &logger,
 	}.NewZerologHandler()))
 
-	if err := internal.Start(8000); err != nil {
+	portStr, exists := os.LookupEnv("PORT")
+	var port uint16 = 8000
+
+	if exists {
+		parsedPort, err := strconv.ParseInt(portStr, 10, 16)
+		if err == nil {
+			port = uint16(parsedPort)
+		} else {
+			slog.Warn("Failed to parse port, using default port 8000", "error", err)
+		}
+	}
+
+	if err := internal.Start(port); err != nil {
 		slog.Error("Error while starting server", "error", err)
 	}
 }
