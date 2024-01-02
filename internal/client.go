fd6527370b008cb17a19c32f0ff97d84d8290a26
mocha8686 <jericho.paulino@yahoo.com.ph>
2024-01-01 19:22:33 -0800

    WriteStringWithLen

--- internal/client.go
+++ internal/client.go
@@ -3,6 +3,8 @@ package internal
 import (
 	"bufio"
 	"io"
+	"log/slog"
+	"math"
 )
 
 type Client struct {
@@ -21,4 +23,22 @@ func (c *Client) ReadString() (string, error) {
 	}
 
 	return string(buf), nil
+}
+
+func (c *Client) WriteStringWithLen(msg string, logger *slog.Logger) error {
+	length := len(msg)
+	if length > math.MaxUint8 {
+		msg = msg[:math.MaxUint8]
+		length = math.MaxUint8
+	}
+
+	if err := c.WriteByte(byte(length)); err != nil {
+		logger.Warn("Failed to write message length to client", "error", err)
+	}
+
+	if _, err := c.WriteString(msg); err != nil {
+		logger.Warn("Failed to write message to client", "error", err)
+	}
+
+	return nil
 }
